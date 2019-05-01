a21883d0fd5022516f948f6b195f2161658b24b2
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2019-05-01 06:29:14 -0700

    consolidated

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -3,8 +3,8 @@
 set -o errexit          # Exit on most errors (see the manual)
 #set -o errtrace         # Make sure any error trap is inherited
 set -o nounset          # Disallow expansion of unset variables
-#set -o pipefail         # Use last non-zero exit code in a pipeline
-set -o xtrace          # Trace the execution of the script (debug)
+set -o pipefail         # Use last non-zero exit code in a pipeline
+#set -o xtrace          # Trace the execution of the script (debug)
 
 DUMP1090_SERVER='dump1090'
 DUMP1090_PORT='30005'
@@ -18,7 +18,7 @@ ping -c 3 "${DUMP1090_SERVER}"
 
 
 echo "Connecting to ${DUMP1090_SERVER}:${DUMP1090_PORT} to send to feed.adsbexchange:31090"
-mlat-client --input-type 'dump1090' \
+exec mlat-client --input-type 'dump1090' \
             --input-connect "${DUMP1090_SERVER}:${DUMP1090_PORT}" \
             --lat "${MLAT_CLIENT_LATITUDE}" \
             --lon "${MLAT_CLIENT_LONGITUDE}" \
